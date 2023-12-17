99de66bfee22bc5c38800d8a0ba49764c6e50d5c
yongkheehou <yongkheehou@gmail.com>
2023-12-17 21:11:50 +0800

    update backend folders

--- backend/controller/user.go
+++ backend/controller/user.go
@@ -2,33 +2,33 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/yongkheehou/cvwo-assignment-repo/backend/config"
+	"github.com/yongkheehou/cvwo-assignment-repo/backend/initializers"
 	"github.com/yongkheehou/cvwo-assignment-repo/backend/models"
 )
 
 func GetUsers(c *gin.Context) {
 	users := []models.User{}
-	config.DB.Find(&users)
+	initializers.DB.Find(&users)
 	c.JSON(200, &users)
 }
 
 func CreateUser(c *gin.Context) {
 	var user models.User
 	c.BindJSON(&user)
-	config.DB.Create(&user)
+	initializers.DB.Create(&user)
 	c.JSON(200, &user)
 }
 
 func DeleteUser(c *gin.Context) {
 	var user models.User
-	config.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	initializers.DB.Where("id = ?", c.Param("id")).Delete(&user)
 	c.JSON(200, &user)
 }
 
 func UpdateUser(c *gin.Context) {
 	var user models.User
-	config.DB.Where("id = ?", c.Param("id")).First(&user) // getting user
+	initializers.DB.Where("id = ?", c.Param("id")).First(&user) // getting user
 	c.BindJSON(&user)
-	config.DB.Save(&user)
+	initializers.DB.Save(&user)
 	c.JSON(200, &user)
 }

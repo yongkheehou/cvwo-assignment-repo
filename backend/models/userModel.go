d2a0a0056a7453cba302d5a8f6705be2eb354d5e
yongkheehou <yongkheehou@gmail.com>
2023-12-19 11:14:16 +0800

    update user model

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -5,8 +5,10 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model // adds ID, CreatedAt, UpdatedAt, DeletedAt
 	// Id         int    `json:"ID" gorm:"primary_key"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
-	// AvatarImage
+	Username   string `gorm:"not null; unique" json:"username"`
+	Password   string `gorm:"not null" json:"password"`
+	ProfilePic string `json:"profilePicture"`
+	// Posts
+	// Comments
+	// Likes
 }

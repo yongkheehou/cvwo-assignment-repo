d60c000ab3e790348aaf56be5617e4892b62647d
yongkheehou <yongkheehou@gmail.com>
2023-12-21 14:47:46 +0800

    allow cors

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/yongkheehou/cvwo-assignment-repo/backend/initializers"
 	"github.com/yongkheehou/cvwo-assignment-repo/backend/routes"
@@ -13,6 +14,13 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://0.0.0.0"},
+		AllowHeaders:     []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+		AllowCredentials: true,
+	}))
+
 	routes.UserRoute(r)
 
 	r.Run(":4000") // listen and serve on 0.0.0.0:4000

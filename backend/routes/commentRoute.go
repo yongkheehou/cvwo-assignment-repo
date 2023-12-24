ccbadf876677266bd23b617886a0538b18d3c875
yongkheehou <yongkheehou@gmail.com>
2023-12-24 15:13:54 +0800

    fix comment routes

--- backend/routes/commentRoute.go
+++ backend/routes/commentRoute.go
@@ -6,9 +6,9 @@ import (
 )
 
 func CommentRoute(r *gin.Engine) {
-	r.GET("/getcomments", controllers.GetAllThreads)
-	r.GET("/comment/:id", controllers.GetSingleThread)
-	r.POST("/createcomment", controllers.CreateThread)
-	r.DELETE("/comment/:id", controllers.DeleteThread)
-	r.PUT("/comment/:id", controllers.UpdateThread)
+	r.GET("/getcomments", controllers.GetAllComments)
+	r.GET("/comment/:id", controllers.GetSingleComment)
+	r.POST("/createcomment", controllers.CreateComment)
+	r.DELETE("/comment/:id", controllers.DeleteComment)
+	r.PUT("/comment/:id", controllers.UpdateComment)
 }

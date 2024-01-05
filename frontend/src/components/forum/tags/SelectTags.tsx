5b1da3bd48cdeb792452f3a34a088d3d5ede2972
yongkheehou <yongkheehou@gmail.com>
2024-01-05 17:15:22 +0900

    search existing tags

--- frontend/src/components/forum/tags/SelectTags.tsx
+++ frontend/src/components/forum/tags/SelectTags.tsx
@@ -2,26 +2,23 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
-
-interface Film {
-  title: string;
-  year: number;
-}
-
-function sleep(duration: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, duration);
-  });
-}
+import { useAppDispatch } from '../../../hooks/reduxHooks';
+import { getAllTags } from '../../../features/forum/TagSlice';
+import { useState, useEffect } from 'react';
+import { Tag } from '../../../features/forum/ForumModels';
 
 export default function SearchTags() {
-  const [open, setOpen] = React.useState(false);
-  const [options, setOptions] = React.useState<readonly Film[]>([]);
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState<readonly Tag[]>([]);
   const loading = open && options.length === 0;
 
-  React.useEffect(() => {
+  const dispatch = useAppDispatch();
+
+  async function getTags() {
+    return await dispatch(getAllTags()).unwrap();
+  }
+
+  useEffect(() => {
     let active = true;
 
     if (!loading) {
@@ -29,10 +26,10 @@ export default function SearchTags() {
     }
 
     (async () => {
-      await sleep(1e3); // For demo purposes.
+      const tags: Tag[] = await getTags(); // For demo purposes.
 
       if (active) {
-        setOptions([...topFilms]);
+        setOptions([...tags]);
       }
     })();
 
@@ -41,7 +38,7 @@ export default function SearchTags() {
     };
   }, [loading]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!open) {
       setOptions([]);
     }
@@ -58,8 +55,8 @@ export default function SearchTags() {
       onClose={() => {
         setOpen(false);
       }}
-      isOptionEqualToValue={(option, value) => option.title === value.title}
-      getOptionLabel={(option) => option.title}
+      isOptionEqualToValue={(option, value) => option.Title === value.Title}
+      getOptionLabel={(option) => option.Title}
       options={options}
       loading={loading}
       renderInput={(params) => (
@@ -82,54 +79,3 @@ export default function SearchTags() {
     />
   );
 }
-
-// Top films as rated by IMDb users. http://www.imdb.com/chart/top
-const topFilms = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-];
